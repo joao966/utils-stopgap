86b203eb3237911c2599c174042f486b7987a01e
joao966 <joaooliveira966@gmail.com>
2023-04-26 18:49:23 -0300

    ADD: create parsing service

---
+++ src/modules/parsing/parsing.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateParsingDto } from './dto/create-parsing.dto';
+import { UpdateParsingDto } from './dto/update-parsing.dto';
+
+@Injectable()
+export class ParsingService {
+  create(createParsingDto: CreateParsingDto) {
+    return `This action returns a create`;
+  }
+
+  findAll() {
+    return `This action returns a findAll`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} parsing`;
+  }
+
+  update(id: number, updateParsingDto: UpdateParsingDto) {
+    return `This action updates a #${id} parsing`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} parsing`;
+  }
+}
