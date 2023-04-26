b36a109b427078fdaf686c7635f124266edf53f7
joao966 <joaooliveira966@gmail.com>
2023-04-26 18:51:20 -0300

    ADD:  import parsing module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParsingModule } from './modules/parsing/parsing.module';
 
 @Module({
-  imports: [],
+  imports: [ParsingModule],
   controllers: [AppController],
   providers: [AppService],
 })
